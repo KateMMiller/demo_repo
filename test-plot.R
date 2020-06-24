2e808d727fc3c88bc400390bf12f3b03d6477507
KateMMiller <kathryn_miller@nps.gov>
2020-06-24 15:18:01 -0400

    add theme_IMD() source

--- test-plot.R
+++ test-plot.R
@@ -1,4 +1,5 @@
 # Code shamelessly plagiarized from http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html#Scatterplot
+source("theme_IMD.R")
 
 library(ggplot2)
 data("midwest", package = "ggplot2")
@@ -8,4 +9,4 @@ data("midwest", package = "ggplot2")
 gg <- ggplot(midwest, aes(x=area, y=poptotal)) + 
   geom_point(aes(col=state, size=popdensity)) 
 
-gg
+gg+theme_IMD()
