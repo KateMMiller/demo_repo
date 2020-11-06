863d84ce64a50956737c9db288fd1737c3aa994e
Judd Patterson <judd_patterson@nps.gov>
2020-11-06 13:36:22 -0500

    simplified the palette print and added comments to clarify interpolation and testing stesps

--- demo-im-palette.R
+++ demo-im-palette.R
@@ -1,16 +1,15 @@
 library(pals)
 source('im-palette.R')
 
-# See palette names
-palette_options <- names(im_palettes)
-palette_options
+# Display a list of available palette names
+print(names(im_palettes))
 
-# Interpolate palettes
-nps_5 <- im_pal()(5)
-dry_to_wet_5 <- im_pal('DRY TO WET')(5)
-dry_to_wet_10 <- im_pal('DRY TO WET')(10)
+# Interpolate colors and save those to variables
+dry_to_wet_5 <- im_pal('DRY TO WET')(5)     #generate 5 colors from 'DRY TO WET' palette
+dry_to_wet_10 <- im_pal('DRY TO WET')(10)   #generate 10 colors from 'DRY TO WET' palette
+nps_5 <- im_pal()(5)                        #generate 5 colors from the default palette, which is 'NPS'
 
-# View palettes
-pal.test(nps_5)
+# Test the look of each set of colors
 pal.test(dry_to_wet_5)
 pal.test(dry_to_wet_10)
+pal.test(nps_5)
